b3d89951e6cec68598f63a2969f0e9ca3526a770
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-04-28 14:16:32 +0200

    Upgrade auf neue Ionic-Version mit Angular 18

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.mide.ionic.wuerfel',
-  appName: 'Würfel',
+  appId: 'io.ionic.starter',
+  appName: 'wuerfel',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,114 +2,109 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 
-
-/**
- * Page zum Abruf der einzelnen Würfelwerten.
- */
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: false,
 })
 export class HomePage {
 
-  /** URL um die Web-API aufzurufen, so dass sie eine Zufallszahl vom Typ `uint8`. */
-  readonly URL_WEBAPI = "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8";
+    /** URL um die Web-API aufzurufen, so dass sie eine Zufallszahl vom Typ `uint8`. */
+    readonly URL_WEBAPI = "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8";
 
-  /** Konfigurations-Objekt für HttpClient. */
-  readonly OPTIONS_OBJECT: object = { observe: "response" };
-
-  /** Wert für "src"-Attribut für `ion-img`-Element, mit dem aktuelle Zufallszahl dargestellt wird. */
-  public wuerfelBildSource = "./assets/wuerfel_1.png";
-
-  /** Würfel wird nur dann angezeigt, wenn diese Variable den Wert `true` hat. */
-  public wuerfelSichtbar = false;
+    /** Konfigurations-Objekt für HttpClient. */
+    readonly OPTIONS_OBJECT: object = { observe: "response" };
 
 
-  /**
-   * Konstruktor für *Dependency Injection*.
-   */
-  constructor( private httpClient: HttpClient,
-               private alertCtrl : AlertController ) {}
+    /** Wert für "src"-Attribut für `ion-img`-Element, mit dem aktuelle Zufallszahl dargestellt wird. */
+    public wuerfelBildSource = "./assets/wuerfel_1.png";
 
+    /** Würfel wird nur dann angezeigt, wenn diese Variable den Wert `true` hat. */
+    public wuerfelSichtbar = false;
+
+    /**
+     * Konstruktor für *Dependency Injection*.
+     */
+    constructor( private httpClient: HttpClient,
+                private alertCtrl : AlertController ) {}
 
   /**
    * Event-Handler für Button, der Web-API-Request auslöst.
    */
-  public onNeueZahlButton() {
+      public onNeueZahlButton() {
 
-      this.httpClient.get(this.URL_WEBAPI, this.OPTIONS_OBJECT)
-                     .subscribe({
-                        next: (antwort) => this.verarbeiteHttpResponse(antwort),
-                        error: (fehler) => this.verarbeiteHttpFehler(fehler)
-                      });
-  }
-
-
-  /**
-   * Event-Handler für erfolgreichen HTTP-Request.
-   * Holt Zufallszahl aus JSON-String in der HTTP-Response und wandelt diese in eine Würfelzahl
-   * von 1..6 um. Die entsprechende Würfelzahl wird als Bild angezeigt.
-   *
-   * Beispiel für Response:
-   * ```
-   * {
-   *   "type":"uint8",
-   *   "length":1,
-   *   "data":[56],
-   *   "success":true
-   * }
-   * ```
-   */
-  private verarbeiteHttpResponse = (httpResponse:any) => {
-
-    const dataArray = httpResponse.body.data;
-
-    const laenge = dataArray.length;
-    if (laenge !== 1) {
-
-      console.log(`FEHLER: Nicht genau ein Element im Array mit Zufallszahlen enthalten sondern ${laenge}.`);
-      return;
+        this.httpClient.get(this.URL_WEBAPI, this.OPTIONS_OBJECT)
+                       .subscribe({
+                          next: (antwort) => this.verarbeiteHttpResponse(antwort),
+                          error: (fehler) => this.verarbeiteHttpFehler(fehler)
+                        });
     }
 
-    const zufallszahl = dataArray[0];
-    console.log(`zufallszahl=${zufallszahl}`);
 
-    const wuerfelzahl = ( zufallszahl % 6 ) + 1;
-    console.log(`wuerfelzahl=${wuerfelzahl}`);
+    /**
+     * Event-Handler für erfolgreichen HTTP-Request.
+     * Holt Zufallszahl aus JSON-String in der HTTP-Response und wandelt diese in eine Würfelzahl
+     * von 1..6 um. Die entsprechende Würfelzahl wird als Bild angezeigt.
+     *
+     * Beispiel für Response:
+     * ```
+     * {
+     *   "type":"uint8",
+     *   "length":1,
+     *   "data":[56],
+     *   "success":true
+     * }
+     * ```
+     */
+    private verarbeiteHttpResponse = (httpResponse:any) => {
 
-    this.wuerfelBildSource = `./assets/wuerfel_${wuerfelzahl}.png`;
-    this.wuerfelSichtbar = true;
-  }
+      const dataArray = httpResponse.body.data;
 
+      const laenge = dataArray.length;
+      if (laenge !== 1) {
 
-  /**
-   * Event-Handler für erfolglosen HTTP-Request.
-   */
-  private verarbeiteHttpFehler = (fehler: HttpErrorResponse) => {
+        console.log(`FEHLER: Nicht genau ein Element im Array mit Zufallszahlen enthalten sondern ${laenge}.`);
+        return;
+      }
 
-    this.zeigeDialog("Fehler bei Web-API-Zugriff um Zufallszahl zu holen.");
+      const zufallszahl = dataArray[0];
+      console.log(`zufallszahl=${zufallszahl}`);
 
-    const fehlerJson = JSON.stringify(fehler);
-    console.log(`Fehler beim Zugriff auf Web-API (Status Code: ${fehlerJson})`);
+      const wuerfelzahl = ( zufallszahl % 6 ) + 1;
+      console.log(`wuerfelzahl=${wuerfelzahl}`);
 
-    this.wuerfelSichtbar = false;
-  }
-
-
-  /**
-   * Hilfsmethode zur Anzeige eines Dialogs.
-   */
-  private async zeigeDialog(nachricht: string) {
-
-      const meinAlert =
-      await this.alertCtrl.create({
-          header  : "Fehler",
-          message : nachricht,
-          buttons : [ "Ok" ]
-      });
-
-      await meinAlert.present();
+      this.wuerfelBildSource = `./assets/wuerfel_${wuerfelzahl}.png`;
+      this.wuerfelSichtbar = true;
     }
 
+
+    /**
+     * Event-Handler für erfolglosen HTTP-Request.
+     */
+    private verarbeiteHttpFehler = (fehler: HttpErrorResponse) => {
+
+      this.zeigeDialog("Fehler bei Web-API-Zugriff um Zufallszahl zu holen.");
+
+      const fehlerJson = JSON.stringify(fehler);
+      console.log(`Fehler beim Zugriff auf Web-API (Status Code: ${fehlerJson})`);
+
+      this.wuerfelSichtbar = false;
+    }
+
+
+    /**
+     * Hilfsmethode zur Anzeige eines Dialogs.
+     */
+    private async zeigeDialog(nachricht: string) {
+
+        const meinAlert =
+        await this.alertCtrl.create({
+            header  : "Fehler",
+            message : nachricht,
+            buttons : [ "Ok" ]
+        });
+
+        await meinAlert.present();
+      }
 }
