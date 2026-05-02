1c79a05a87fb813b671c783710f32db9715f0cd6
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-02 20:01:23 +0200

    Aenderungen in capacitor.config.ts und home.page.scss nachgeliefert

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'wuerfel',
+  appId: 'de.mide.ionic.wuerfelapp',
+  appName: 'Wuerfel',
   webDir: 'www'
 };
 
